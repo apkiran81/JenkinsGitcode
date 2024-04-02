81600d12e4bca3c8caeee6a1067a886eee460824
apkiran81 <164561442+apkiran81@users.noreply.github.com>
2024-04-02 14:17:01 -0400

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+		printHello();
+	}
+	
+	public static void printHello(){
+		System.out.println("Hello World....");
+	}
+
+}
